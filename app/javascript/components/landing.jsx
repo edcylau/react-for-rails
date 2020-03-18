88fa402a7ce9bfcbadc624ba15a5ea40ab82f140
Edmund Lau <tmlcyaa@gmail.com>
2020-03-18 14:03:34 +0800

    second commit before yarn add

---
+++ app/javascript/components/landing.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>Hello from Home component!</div>
+    );
+  }
+}
+
+export default Landing;
